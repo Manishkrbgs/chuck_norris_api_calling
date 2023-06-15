33c88505feb398bc306040436a1d11048d5242cb
Manish kumar <mrazz938@gmail.com>
2023-06-15 20:46:42 +0530

    api_fetch1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,18 +2,68 @@ package org.example;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args) throws Exception{
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        String urlString = "https://api.chucknorris.io/jokes/random";
+
+        // creating a URL
+        URL url = new URL(urlString);
+
+        // Creating a connection
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        // Getting response code
+        int responseCode = connection.getResponseCode();
+
+//        Extracting Information from Byte Codes into JSON
+
+//        Extracting information from BufferReader. BufferReader has data in
+//        the form of packages or Byte codes after calling the API. After, we have to
+//        convert that byte codes into readable data (basically into JSON format).
+//        We need to create a StringBuilder and keep appending the data after converting into
+//        JSON into StringBuilder.
+
+        if(responseCode == 200){
+            BufferedReader byteCodes = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuilder apiData = new StringBuilder();
+            String readline = null;
+
+            while((readline = byteCodes.readLine()) != null){
+                apiData.append(readline);
+            }
+
+            byteCodes.close();
+
+//            To convert String to JSON
+            JSONObject jsonAPIResponse = new JSONObject(apiData.toString());
+
+            System.out.println(jsonAPIResponse);
+            System.out.println(jsonAPIResponse.get("icon_url"));
+            System.out.println(jsonAPIResponse.get("updated_at"));
+            System.out.println(jsonAPIResponse.get("created_at"));
+            System.out.println(jsonAPIResponse.get("id"));
+            System.out.println(jsonAPIResponse.get("value"));
+            System.out.println(jsonAPIResponse.get("url"));
+            System.out.println(jsonAPIResponse.get("categories"));
         }
+        else
+            System.out.println("API call could not be made!!!");
+
+
+
+
+
+
+
+
     }
 }
